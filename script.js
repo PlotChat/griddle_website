740c062b6d1d91141f99e48f6dda7721d1fe2a09
PlotChat <levietanh1234567@gmail.com>
2025-12-05 00:24:00 +0700

    Add code for Grid - Add function to display grid items - Add object to set grid settings

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ if (!theme) theme = "dark";
 
 document.documentElement.setAttribute("data-theme", theme);
 
+
+// Grid Settings
+grid = {
+    gridSize: 100,
+}
+
+// Display grid items by multiplying one
+function displayGridItem(){
+    const gridEl = document.getElementById("grid");
+
+    gridEl.style.gridTemplateColumns = `repeat(${grid.gridSize}, max(0.4%, 0.2rem)`;
+    gridEl.style.gridTemplateRows = `repeat(${grid.gridSize}, auto-fill)`;
+
+    const gridItem = gridEl.querySelector(".grid-item");
+
+    for(i = 0; i < grid.gridSize*grid.gridSize - 1; i++){
+        const clone = gridItem.cloneNode(true);
+        gridEl.appendChild(clone);
+    }
+}
+
+displayGridItem();
+
